548143808b16e886361e03d59d4fa00b2b74f8f4
XAnthekiraX <alexbalverde123@gmail.com>
2024-06-01 22:10:26 -0500

    updated

---
+++ rentxpress/src/components/home/loader/main-load.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image'; 
+import street from '../../../images/png/loader.png'; // Asegúrate de que la ruta es correcta
+import logo from '../../../images/svg/logo.svg'; // Asegúrate de que la ruta es correcta
+
+const loader: React.FC = () => {
+    return (
+      // Contenedor de todos los elementos
+      <div id='loader' className="h-auto w-full flex items-center justify-center overflow-hidden relative z-20">
+        {/* progress bar y imagen */}
+        <div className='absolute z-10 w-3/4 h-auto flex flex-col justify-center items-center'>
+          {/* imagen */}
+        <Image
+        src={logo}
+        alt=''
+        className='w-1/2 max-w-[900px]'
+        /> 
+        <div className="bar-loader max-w-[30%] w-full h-2 rounded-lg  overflow-hidden mt-8 bg-white">
+        {/* Progress bar */}
+          <div className="charger h-full w-1/2 rounded-lg bg-blue-400"></div>
+        </div>
+        </div>
+        <div className="opacity w-full h-full absolute top-0 left-0 opacity-[60%]"></div>
+        <Image 
+          src={street} 
+          alt="Descripción de la imagen"  
+          className="min-w-[100%] w-[1490px] h-screen"
+        />
+      </div>
+    );
+  };
+  
+  export default loader;
---
+++ rentxpress/src/pages/pagina3.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pagina3: React.FC = () => {
+  return (
+    <div className='bg-black'>
+      <h1>Página 3</h1>
+    </div>
+  );
+};
+
+export default Pagina3;
